3a5bf205720b0e0edc3db31f7aa0518c1138a4e7
Fishy Boi <mcphersond13@gmail.com>
2018-04-07 00:16:30 -0400

    I changed some stuff.
    It was some reaaaally good changes.

--- app/src/main/java/com/chandlershax/backseatdj/MainActivity.java
+++ app/src/main/java/com/chandlershax/backseatdj/MainActivity.java
@@ -2,6 +2,8 @@ package com.chandlershax.backseatdj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnTest_OnClick(View v)
+    {
+
+            TextView tv = findViewById(R.id.txtWorld);
+            tv.setText("Horatio Caine was a great character in CSI Miami");
+
+
     }
 }
